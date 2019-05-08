c4240d9fd1a2b7c2c0910421e0e5dc8627f23f1f
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-05-08 22:05:06 +0100

    Admin can perform search on other users

--- Users/Admin.js
+++ Users/Admin.js
@@ -1,5 +1,6 @@
 var User = require('./User');
 var InheritProperty = require('./Interface/InheritProperty');
+var databaseHandler = require('../Library/database/Database');
 
 function Admin(fullName) {
   User.call(this, fullName, 'ADMIN');
@@ -8,6 +9,15 @@ InheritProperty(User, Admin);
 
 Admin.prototype.updateFullName = function(fullName) {
   this.update(fullName);
+};
+
+Admin.prototype.searchUserByID = function(id) {
+  var users = databaseHandler['users'];
+  for (var index = 0; index < users.length; index++) {
+    if (users[index].id === id) {
+      return users[index];
+    }
+  }
 };
 
 module.exports = Admin;
--- Users/Admin.test.js
+++ Users/Admin.test.js
@@ -1,4 +1,6 @@
 var Admin = require('./Admin');
+var Teacher = require('./Teacher');
+var Student = require('./Student');
 
 test('Admin can be created', function() {
   var admin = new Admin('Izuking Ogbodo');
@@ -19,4 +21,31 @@ test('Admin account can be deleted', function() {
 test('Admin details can be read', function() {
   var admin = new Admin('Matthias Ogbonna');
   expect(admin.retrieveDetails()).toEqual(admin);
+});
+
+describe('Admin can perform search on other users', function() {
+  var admin = new Admin('Matthias Ogbonna');
+
+  test('For the case student', function() {
+    var student = new Student(
+      'Solomon Izukerberg',
+      '2041200015',
+      'Male',
+      'Science',
+      'Mathematics',
+      '200L'
+    );
+
+    expect(admin.searchUserByID(student.id)).toEqual(student);
+  });
+  test('For the case teacher', function() {
+    var teacher = new Teacher(
+      'Mr. David Mogbeyi',
+      'Male',
+      'Dev/1/340',
+      'Science',
+      'Computer Science'
+    );
+    expect(admin.searchUserByID(teacher.id)).toEqual(teacher);
+  });
 });
