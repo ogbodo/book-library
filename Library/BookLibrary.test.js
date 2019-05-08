e41f8d5e54e32fe5304c2558a905b9e2c997267a
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-05-08 16:36:47 +0100

    Minor modification

--- Library/BookLibrary.test.js
+++ Library/BookLibrary.test.js
@@ -1,6 +1,6 @@
 var BookLibrary = require('./BookLibrary');
 
-test('Teacher can be created', function() {
+test('Books can be created', function() {
   var bookLibrary = new BookLibrary(
     'What Men Want',
     'Journal',
