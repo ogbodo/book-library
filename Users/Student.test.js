70899de031d37ccc23f6cc1d7c9d47740d8afe8c
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-05-09 14:18:24 +0100

    Student details can be updated

--- Users/Student.test.js
+++ Users/Student.test.js
@@ -1,14 +1,39 @@
 var Student = require('./Student');
 
-test('Student can be created', function() {
-  var student = new Student(
-    'Solomon',
-    'Izukerberg',
-    '2041200015',
-    'Male',
-    'Science',
-    'Mathematics',
-    '200L'
-  );
-  expect(student.getFirstName()).toBe('Solomon');
+//Functionalities of Admin with respect to own account
+describe('All about Admin own account functionalities', function() {
+  test('Student can be created', function() {
+    var student = new Student(
+      'Solomon',
+      'Izukerberg',
+      '2041200015',
+      'Male',
+      'Science',
+      'Mathematics',
+      '200L'
+    );
+    expect(student.getFirstName()).toBe('Solomon');
+  });
+
+  describe('Student details can be updated', function() {
+    var student = new Student(
+      'Samuel',
+      'Micheal',
+      '7782310091',
+      'Male',
+      'Science',
+      'Chemistry',
+      '300L'
+    );
+
+    test('For the case of first name', function() {
+      student.updateFirstName('Ebuka');
+      expect(student.getFirstName()).toBe('Ebuka');
+    });
+
+    // test('For the case of last name', function() {
+    //   admin.updateLastName('Ade');
+    //   expect(admin.getLastName()).toBe('Ade');
+    // });
+  });
 });
