9b9a9d5e179b3b315e0ea9ead3726bc8eebd942e
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-05-09 16:48:47 +0100

    Teacher can update their name

--- Users/Teacher.test.js
+++ Users/Teacher.test.js
@@ -1,13 +1,31 @@
 var Teacher = require('./Teacher');
 
-test('Teacher can be created', function() {
-  var teacher = new Teacher(
-    'David',
-    'Mogbeyi',
-    'Dev/1/340',
-    'Male',
-    'Science',
-    'Computer Science'
-  );
-  expect(teacher.getFirstName()).toBe('David');
+//Functionalities of Teacher with respect to own account
+describe('All about Admin own account functionalities', function() {
+  test('Teacher can be created', function() {
+    var teacher = new Teacher(
+      'David',
+      'Mogbeyi',
+      'Dev/1/340',
+      'Male',
+      'Science',
+      'Computer Science'
+    );
+    expect(teacher.getFirstName()).toBe('David');
+  });
+  describe('Student details can be updated', function() {
+    var teacher = new Teacher(
+      'Ayo',
+      'Abidemi',
+      'Dev/16/340',
+      'Female',
+      'Social Science',
+      'Political Science'
+    );
+
+    test('For the case of first name', function() {
+      teacher.updateFirstName('Fola');
+      expect(teacher.getFirstName()).toBe('Fola');
+    });
+  });
 });
